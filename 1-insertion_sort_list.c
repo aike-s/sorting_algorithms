f414a0a4b6c1c06377c11cc13f35c352a417f0fc
lemejiamo <luismejia69@gmail.com>
2021-06-13 09:07:50 -0500

    fixed bug in line 26

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -23,14 +23,12 @@ void insertion_sort_list(listint_t **list)
 	        print_list(*list);
 
             /* loop to change the minor elements (loop backward <-) */
-            for (previous_node = current_node; previous_node->n < previous_node->prev->n; previous_node = previous_node->prev)
+            for (previous_node = current_node; previous_node->n < ((previous_node->prev)?previous_node->prev->n:0); previous_node = previous_node->prev)
             {
                 tmp = previous_node->prev->n;
                 previous_node->prev->n = previous_node->n;
                 previous_node->n = tmp;
                 print_list(*list);
-                if (previous_node->prev == NULL)
-                    break;
             }
 
         }
