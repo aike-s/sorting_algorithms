ac9d577e7c45799c3de52b2b151e94b0ea19d3e4
lemejiamo <luismejia69@gmail.com>
2021-06-13 08:47:02 -0500

    comments added

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,19 +5,24 @@
 */
 void insertion_sort_list(listint_t **list)
 {
+    /* declaration of vars */
     listint_t *current = NULL, *anterior=NULL;
     int tmp = 0;
 
+    /* node to roam the list */
     current = *list;
-
+    /*loop to roam the list (loop forward)*/
     for (;current->next != NULL; current=current->next)
     {
+        /* change condition (fordward ->)*/
         if (current->n > current->next->n)
         {
             tmp = current->next->n;
             current->next->n = current->n;
             current->n = tmp;
 	        print_list(*list);
+
+            /* loop to change the minor elements (loop backward <-) */
             for (anterior = current; anterior->n < anterior->prev->n; anterior = anterior->prev)
             {
                 tmp = anterior->prev->n;
